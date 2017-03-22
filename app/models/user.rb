ddc972d4bc54dfd2f598f5f7924e762bf44ccd26
heric <hericpeng@gmail.com>
2017-03-23 06:24:08 +0800

    join or quit movie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :movies
-  has_many :reviews
-  has_many :favorites
-  has_many :favorited_movies, :through => :favorites, :source => :movie
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
+    has_many :movies
+    has_many :reviews
+    has_many :favorites
+    has_many :favorited_movies, through: :favorites, source: :movie
 
-  def is_fan_of?(movie)
-     favorited_movies.include?(movie)
-  end
+    def is_fan_of?(movie)
+        favorited_movies.include?(movie)
+    end
+
+    def join!(movie)
+        favorited_movies << movie
+    end
+
+    def quit!(movie)
+        favorited_movies.delete(movie)
+    end
 end
