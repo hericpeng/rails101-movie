cf2bb19ede30c9a1961c6a8d91e268aaa8883c37
heric <hericpeng@gmail.com>
2017-03-22 21:24:19 +0800

    implement movies#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,26 @@
 class MoviesController < ApplicationController
-  def index
-  @movies = Movie.all
-  end
-  def new
-    @movie = Movie.new
-  end
-   def create
-   @movie = Movie.new(movie_params)
-   @movie.save
+    def index
+        @movies = Movie.all
+    end
 
-     redirect_to movies_path
- end
+    def new
+        @movie = Movie.new
+    end
+    
+    def show
+      @movie = Movie.find(params[:id])
+    end
 
- private
- def movie_params
-   params.require(:movie).permit(:title, :description)
- end
+    def create
+        @movie = Movie.new(movie_params)
+        @movie.save
+
+        redirect_to movies_path
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title, :description)
+    end
 end
