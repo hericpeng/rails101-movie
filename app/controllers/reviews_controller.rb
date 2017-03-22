1126433ccdba135ca890f255d019681f04000d74
heric <hericpeng@gmail.com>
2017-03-23 07:53:06 +0800

    修改 favorite button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,54 +1,51 @@
 class ReviewsController < ApplicationController
+    before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
-  before_action :authenticate_user!, only:[:edit, :update,:destroy]
-
-  def new
-    @movie = Movie.find(params[:movie_id])
-    @review = Review.new
-  end
-
-  def create
-    @movie = Movie.find(params[:movie_id])
-    @review = Review.new(review_params)
-    @review.movie = @movie
-    @review.user = current_user
-
-    if @review.save
-      redirect_to movie_path(@movie)
-    else
-      render :new
+    def new
+        @movie = Movie.find(params[:movie_id])
+        @review = Review.new
     end
-  end
 
-  def edit
+    def create
+        @movie = Movie.find(params[:movie_id])
+        @review = Review.new(review_params)
+        @review.movie = @movie
+        @review.user = current_user
+
+        if @review.save
+            redirect_to movie_path(@movie)
+        else
+            render :new
+        end
+    end
+
+    def edit
         @movie = Movie.find(params[:movie_id])
         @review = Review.find(params[:id])
         if current_user != @review.user
-          redirect_to account_reviews_path, alert:"You have no permission."
+            redirect_to account_reviews_path, alert: 'You have no permission.'
         end
-    end
+      end
+
     def update
-     @movie = Movie.find(params[:movie_id])
-     @review = Review.find(params[:id])
-     if @review.update(review_params)
-       redirect_to movies_path, notice:"Review Update Success"
-     else
-       render :edit
-     end
+        @movie = Movie.find(params[:movie_id])
+        @review = Review.find(params[:id])
+        if @review.update(review_params)
+            redirect_to movies_path, notice: 'Review Update Success'
+        else
+            render :edit
+        end
    end
 
-   def destroy
-     @movie = Movie.find(params[:movie_id])
-     @review = Review.find(params[:id])
-     if @review.destroy
-       redirect_to movies_path, alert:"Review Deleted"
-     end
-   end
+    def destroy
+        @movie = Movie.find(params[:movie_id])
+        @review = Review.find(params[:id])
+        redirect_to movies_path, alert: 'Review Deleted' if @review.destroy
+    end
 
-  private
+    private
 
-  def review_params
-    params.require(:review).permit(:content)
-  end
-
+    def review_params
+        params.require(:review).permit(:content)
+    end
 end
